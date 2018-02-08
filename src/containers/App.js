e3f00a337f29589dc59d163d044d86aa308fe9e1
Muhammad Edward Chakim <muhedwardc@gmail.com>
2018-02-08 21:27:06 +0700

    cleaning codes

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,23 +21,22 @@ class App extends Component {
     }
 
     render () {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
 
-        let content = (
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <ScrollContent>
-                    <CardList robots={filteredRobots} />
-                </ScrollContent>
-            </div>
-        )
-
-        if (this.state.robots === 0) content = <h1>Loading</h1>
-
-        return content;
+        return !robots.length 
+            ? <h1 className='tc'>Loading</h1>
+            : (
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <ScrollContent>
+                        <CardList robots={filteredRobots} />
+                    </ScrollContent>
+                </div>
+            )
     }
 }
 
