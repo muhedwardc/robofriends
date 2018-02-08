6a1273469a18b082fa14e2912cc012273b9440f2
Muhammad Edward Chakim <muhedwardc@gmail.com>
2018-02-08 20:27:38 +0700

    adding styles to component

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './SearchBox.css';
 
 const SearchBox = (props) => {
     return (
         <div className='pa2'>
             <input 
-                className='pa3 ba b--green bg-lightest-blue'
+                className='search-bar pa3 ba b--green bg-lightest-blue'
                 type='search' 
                 placeholder='search robots'
                 onChange={props.searchChange} />
